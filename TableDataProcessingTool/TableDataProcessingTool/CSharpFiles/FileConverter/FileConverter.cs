19a755130fd7406497def83382019de1e0c63385
JimmyHu <60071jimmy@gmail.com>
2025-01-31 20:12:26 +0800

    Add FileConverter.cs
    Add FileConverter.cs

---
+++ TableDataProcessingTool/TableDataProcessingTool/CSharpFiles/FileConverter/FileConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableDataProcessingTool.CSharpFiles
+{
+	public class FileConverter
+	{
+		public FileConverter(string Filename, string OutputFolder)
+		{
+			string OutputFilename =
+				OutputFolder +
+				"\\" + System.IO.Path.GetFileNameWithoutExtension(Filename) + ".sheets";
+
+			new CSharpFiles.SheetFileStructure(Filename).Save(OutputFilename);
+		}
+	}
+}
