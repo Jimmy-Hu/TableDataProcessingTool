e11fb90ee59270382cf30f36fa3de508cd7d25e7
JimmyHu <60071jimmy@gmail.com>
2025-02-01 21:36:44 +0800

    Update BaseClass.cs
    Update BaseClass.cs

--- TableDataProcessingTool/TableDataProcessingTool/CSharpFiles/BaseClass/BaseClass.cs
+++ TableDataProcessingTool/TableDataProcessingTool/CSharpFiles/BaseClass/BaseClass.cs
@@ -7,64 +7,64 @@ using System.Threading.Tasks;
 
 namespace TableDataProcessingTool.CSharpFiles
 {
-	[Serializable()]
-	public class BaseClass
-	{
-		//	Reference: https://stackoverflow.com/a/222761/6667035
-		public static object DeepClone(object obj)
-		{
-			object objResult = null;
+    [Serializable()]
+    public class BaseClass
+    {
+        //    Reference: https://stackoverflow.com/a/222761/6667035
+        public static object DeepClone(object obj)
+        {
+            object objResult = null;
 
-			using (var ms = new System.IO.MemoryStream())
-			{
-				var bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-				bf.Serialize(ms, obj);
+            using (var ms = new System.IO.MemoryStream())
+            {
+                var bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                bf.Serialize(ms, obj);
 
-				ms.Position = 0;
-				objResult = bf.Deserialize(ms);
-			}
-			return objResult;
-		}
+                ms.Position = 0;
+                objResult = bf.Deserialize(ms);
+            }
+            return objResult;
+        }
 
-		//	Reference: https://stackoverflow.com/a/49160189/6667035
-		public object ExecuteWithReflection(string methodName, object classInstanceInput = null, object parameterObject = null)
-		{
-			Type typeInstance = this.GetType();
+        //    Reference: https://stackoverflow.com/a/49160189/6667035
+        public object ExecuteWithReflection(string methodName, object classInstanceInput = null, object parameterObject = null)
+        {
+            Type typeInstance = this.GetType();
 
-			if (typeInstance != null)
-			{
-				System.Reflection.MethodInfo methodInfo = typeInstance.GetMethod(methodName);
-				ParameterInfo[] parameterInfo = methodInfo.GetParameters();
+            if (typeInstance != null)
+            {
+                System.Reflection.MethodInfo methodInfo = typeInstance.GetMethod(methodName);
+                ParameterInfo[] parameterInfo = methodInfo.GetParameters();
 
-				if (classInstanceInput is null)
-				{
-					object classInstance = Activator.CreateInstance(typeInstance, null);
+                if (classInstanceInput is null)
+                {
+                    object classInstance = Activator.CreateInstance(typeInstance, null);
 
-					if (parameterInfo.Length == 0)
-					{
-						// there is no parameter we can call with 'null'
-						return methodInfo.Invoke(classInstance, null);
-					}
-					else
-					{
-						return methodInfo.Invoke(classInstance, new object[] { parameterObject });
-					}
-				}
-				else
-				{
-					if (parameterInfo.Length == 0)
-					{
-						// there is no parameter we can call with 'null'
-						return methodInfo.Invoke(classInstanceInput, null);
-					}
-					else
-					{
-						return methodInfo.Invoke(classInstanceInput, new object[] { parameterObject });
-					}
-				}
-				
-			}
-			return null;
-		}
-	}
+                    if (parameterInfo.Length == 0)
+                    {
+                        // there is no parameter we can call with 'null'
+                        return methodInfo.Invoke(classInstance, null);
+                    }
+                    else
+                    {
+                        return methodInfo.Invoke(classInstance, new object[] { parameterObject });
+                    }
+                }
+                else
+                {
+                    if (parameterInfo.Length == 0)
+                    {
+                        // there is no parameter we can call with 'null'
+                        return methodInfo.Invoke(classInstanceInput, null);
+                    }
+                    else
+                    {
+                        return methodInfo.Invoke(classInstanceInput, new object[] { parameterObject });
+                    }
+                }
+                
+            }
+            return null;
+        }
+    }
 }
