cfb410ec81a60b75ae9a98e447b7cfb79b98657e
JimmyHu <60071jimmy@gmail.com>
2025-02-05 16:24:20 +0800

    Add SettingsItems.cs
    Add SettingsItems.cs

---
+++ TableDataProcessingTool/TableDataProcessingTool/CSharpFiles/SettingsItems/SettingsItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableDataProcessingTool.CSharpFiles.SettingsItems
+{
+    public static class SettingsItems
+    {
+        public static Color backcolor = Color.White;
+        public static Bitmap backgroundImage = null;
+        public static Font font = null;
+    }
+}
