c2a649bf996fb324ff99213116544a6376f6ec44
JimmyHu <60071jimmy@gmail.com>
2025-02-24 23:33:31 +0800

    Update AssemblyInfo.cs
    Update AssemblyInfo.cs

--- TableDataProcessingTool/TableDataProcessingTool/Properties/AssemblyInfo.cs
+++ TableDataProcessingTool/TableDataProcessingTool/Properties/AssemblyInfo.cs
@@ -7,3 +7,4 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("TableDataProcessingTool")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
