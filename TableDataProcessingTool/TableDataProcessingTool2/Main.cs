4f0f0a5b340fde9cd406d83f15b8a2ecd73d242c
JimmyHu <60071jimmy@gmail.com>
2025-04-27 21:56:35 +0800

    Update Main.cs
    Update Main.cs

--- TableDataProcessingTool/TableDataProcessingTool2/Main.cs
+++ TableDataProcessingTool/TableDataProcessingTool2/Main.cs
@@ -5,6 +5,22 @@ namespace TableDataProcessingTool2
         public Main()
         {
             InitializeComponent();
+
+            var titleItem_File = new System.Windows.Forms.ToolStripMenuItem()
+            {
+                Name = "File",
+                Text = "File",
+            };
+            menuStrip1.Items.Add(titleItem_File);
+            var titleItem_File_Open = new System.Windows.Forms.ToolStripMenuItem()
+            {
+                Name = "Open",
+                Text = "Open"
+            };
+            titleItem_File.DropDownItems.Add(titleItem_File_Open);
+            titleItem_File_Open.Click += new EventHandler(delegate (Object o, EventArgs arg)        //  Open option
+            {
+            });
         }
     }
 }
