5bb4356840147ba032514f930c0df545c96680a0
JimmyHu <60071jimmy@gmail.com>
2025-04-28 23:26:49 +0800

    Move files
    Move files

--- TableDataProcessingTool/TableDataProcessingTool2/WinForms/Main.Designer.cs
+++ TableDataProcessingTool/TableDataProcessingTool2/WinForms/Main.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             menuStrip1 = new MenuStrip();
+            groupBox_filelist = new GroupBox();
             SuspendLayout();
             // 
             // menuStrip1
@@ -39,11 +40,21 @@
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
+            // groupBox_filelist
+            // 
+            groupBox_filelist.Location = new Point(12, 27);
+            groupBox_filelist.Name = "groupBox_filelist";
+            groupBox_filelist.Size = new Size(197, 411);
+            groupBox_filelist.TabIndex = 1;
+            groupBox_filelist.TabStop = false;
+            groupBox_filelist.Text = "File List";
+            // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(groupBox_filelist);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "Main";
@@ -55,5 +66,6 @@
         #endregion
 
         private MenuStrip menuStrip1;
+        private GroupBox groupBox_filelist;
     }
 }
