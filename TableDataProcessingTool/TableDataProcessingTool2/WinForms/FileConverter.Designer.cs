081f1cf788ed72f586f4b68deda63f343b37fba8
JimmyHu <60071jimmy@gmail.com>
2025-06-25 14:18:56 +0800

    Update FileConverter.Designer.cs
    Update FileConverter.Designer.cs

--- TableDataProcessingTool/TableDataProcessingTool2/WinForms/FileConverter.Designer.cs
+++ TableDataProcessingTool/TableDataProcessingTool2/WinForms/FileConverter.Designer.cs
@@ -26,5 +26,6 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
+        private void InitializeComponent()
     }
 }
