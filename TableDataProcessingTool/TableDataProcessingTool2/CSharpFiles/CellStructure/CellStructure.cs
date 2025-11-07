970f5fef1a3a14bd2cf26b3466d09a2f84c30c3f
JimmyHu <60071jimmy@gmail.com>
2025-11-07 16:48:02 +0800

    Update CellStructure.cs
    Update CellStructure.cs

--- TableDataProcessingTool/TableDataProcessingTool2/CSharpFiles/CellStructure/CellStructure.cs
+++ TableDataProcessingTool/TableDataProcessingTool2/CSharpFiles/CellStructure/CellStructure.cs
@@ -80,3 +80,4 @@ namespace TableDataProcessingTool2.CSharpFiles
             }
             //    Try to convert to decimal number
             if (decimal.TryParse(InputString, out decimal DecimalNumberParsingResult))
+            {
