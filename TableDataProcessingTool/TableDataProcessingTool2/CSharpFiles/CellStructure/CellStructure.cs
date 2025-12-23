672aa6793a0b3bda6032c6844c59a3c148b500e3
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2025-12-23 16:27:38 +0800

    Update CellStructure.cs
    Update CellStructure.cs

--- TableDataProcessingTool/TableDataProcessingTool2/CSharpFiles/CellStructure/CellStructure.cs
+++ TableDataProcessingTool/TableDataProcessingTool2/CSharpFiles/CellStructure/CellStructure.cs
@@ -128,3 +128,4 @@ namespace TableDataProcessingTool2.CSharpFiles
             //    Try to convert to double number
             double DoubleNumberParsingResult;
             if (double.TryParse(InputString, out DoubleNumberParsingResult))
+            {
