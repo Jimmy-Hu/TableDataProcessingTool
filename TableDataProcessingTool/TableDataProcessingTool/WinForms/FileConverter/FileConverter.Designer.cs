0102337a9a0116ae71953ce92f1e30007d356647
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-02-13 15:13:30 +0800

    Add FileConverter.Designer.cs
    Add FileConverter.Designer.cs

---
+++ TableDataProcessingTool/TableDataProcessingTool/WinForms/FileConverter/FileConverter.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace TableDataProcessingTool
+{
+    partial class FileConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ProcessingInfo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.BrowseFileButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.ProcessingInfo);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ConvertButton);
+            this.groupBox1.Controls.Add(this.BrowseFileButton);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(775, 327);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Convert .xls / .xlsx to .sheets file";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(163, 167);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(606, 25);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // ProcessingInfo
+            // 
+            this.ProcessingInfo.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.ProcessingInfo.Location = new System.Drawing.Point(163, 198);
+            this.ProcessingInfo.Multiline = true;
+            this.ProcessingInfo.Name = "ProcessingInfo";
+            this.ProcessingInfo.ReadOnly = true;
+            this.ProcessingInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ProcessingInfo.Size = new System.Drawing.Size(606, 122);
+            this.ProcessingInfo.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.label2.Location = new System.Drawing.Point(6, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Processing Info:";
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(644, 124);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(125, 37);
+            this.ConvertButton.TabIndex = 3;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // BrowseFileButton
+            // 
+            this.BrowseFileButton.Location = new System.Drawing.Point(644, 27);
+            this.BrowseFileButton.Name = "BrowseFileButton";
+            this.BrowseFileButton.Size = new System.Drawing.Size(125, 34);
+            this.BrowseFileButton.TabIndex = 2;
+            this.BrowseFileButton.Text = "Browse";
+            this.BrowseFileButton.UseVisualStyleBackColor = true;
+            this.BrowseFileButton.Click += new System.EventHandler(this.BrowseFileButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(475, 103);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.label1.Location = new System.Drawing.Point(6, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input file (.xls / .xlsx) :";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(163, 136);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(334, 25);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Put output file in the same folder of input";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // FileConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 354);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FileConverter";
+            this.Text = "FileConverter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Button BrowseFileButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ProcessingInfo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
